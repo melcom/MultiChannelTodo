de8ef3b97fa663a612d7409db0fbccd42a86f429
Pierre-Henri Gache <pierrehenri.gache@outlook.com>
2015-11-13 17:39:15 +0100

    Update ps1

--- src/BuildNDeploy/UpdateParameters.ps1
+++ src/BuildNDeploy/UpdateParameters.ps1
@@ -2,18 +2,29 @@
 # UpdateParameters.ps1
 #
 
-Write-Host (Get-Variable | Out-String)
+$wd = $Env:System.DefaultWorkingDirectory
 
-$parameters = Get-ChildItem $Env:Release.EnvironmentUri -Recurse -Filter "SetParameters.xml"
+$parameters = Get-ChildItem $wd -Recurse -Filter "*SetParameters.xml"
 
 Foreach($p in $parameters) {
 	
-	Write-Host "Updating $p.FullName"
+	Write-Host Updating $p.FullName
 	
-	#$xml = New-Object XML
-	#$xml.Load($manifest.FullName)
-	#$version = "{0}.{1}.{2}.{3}" -f $major, $minor, $revision, $build
-	#Write-Output "Updating to version $version"
-	#$xml.Package.Identity.Version = $version
-	#$xml.Save($manifest.FullName)
+	$xml = New-Object XML
+	$xml.Load($p.FullName)
+
+	foreach($node in $xml.parameters.setParameter)
+    {        
+        $value = Get-Variable $node.name -ValueOnly -ErrorAction SilentlyContinue
+        if($value)
+        {
+            Write-Output ("Setting {0} with value {1}" -f $node.name, $value)
+            $node.SetAttribute('value', $value)
+        }
+        else
+        {
+            Write-Output ("No {0} variable found" -f $node.name)
+        }
+    }
+    $xml.Save($p.FullName)
 }
