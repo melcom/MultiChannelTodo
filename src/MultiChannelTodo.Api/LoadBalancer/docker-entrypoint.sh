9ef94234a7a25d24dcbb4721b51a82892551cb80
Pierre-Henri Gache <pierrehenri.gache@outlook.com>
2016-06-27 22:15:57 +0200

    update haproxy Dockerfile

--- src/MultiChannelTodo.Api/LoadBalancer/docker-entrypoint.sh
+++ src/MultiChannelTodo.Api/LoadBalancer/docker-entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
+service haproxy start
 
 #generate config
-consul-template -consul=$CONSUL_URL -template="/templates/template.cfg:/usr/local/etc/haproxy/haproxy.cfg" -once
-
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-        set -- haproxy "$@"
-fi
-
-if [ "$1" = 'haproxy' ]; then
-        # if the user wants "haproxy", let's use "haproxy-systemd-wrapper" instead so we can have proper reloadability implemented by upstream
-        shift # "haproxy"
-        set -- "$(which haproxy-systemd-wrapper)" -p /run/haproxy.pid "$@"
-fi
-
-exec "$@"
+consul-template -consul=$CONSUL_URL -template="/templates/template.cfg:/etc/haproxy/haproxy.cfg:service haproxy reload"
